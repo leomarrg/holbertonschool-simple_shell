07b14aaeae85c54c3dcbe70c1c8900e07c0843a5
Sebas-crypt <usefullseba17@gmail.com>
2023-12-21 11:49:46 -0400

    checking fixes on the path and env

--- env.c
+++ env.c
@@ -1,94 +1,28 @@
-#include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <signal.h>
-#include <string.h>
-
-#define MAX_INPUT_SIZE 1024
-#define MAX_ARGS 100
+#include <unistd.h>
 /**
- * print_env - prints environment vars
- * @envp: char in use
+ * @environ: global var
  */
-void print_env(char *envp[])
+void clearEnvironmentAndExecuteEnv(void)
 {
-	for (int i = 0; envp[i] != NULL; i++)
+	char **new_env = (char **)malloc(sizeof(char *));
+
+	new_env[0] = NULL;
+
+	environ = new_env;
+
+	char *args[] = {"env", NULL};
+
+	if (execvp("env", args) == -1)
 	{
-		printf("%s\n", envp[i]);
+		perror("execvp");
+		exit(EXIT_FAILURE);
 	}
 }
-/**
- * exeCmd - function that executes the command
- * @command: command to execute
- * @executable: executes
- * @envp: current environment vars
- */
-void exeCmd(char *command, char *executable, char *envp[])
+
+int main(void)
 {
-	pid_t pid, wpid;
-	int status, i;
-
-	pid = fork();
-	if (pid == 0)
-	{
-	else
-		if (pid < 0)
-		{
-			perror("Error forking");
-			exit(EXIT_FAILURE);
-		}
-		else
-		{
-			do {
-				wpid = wait(&status);
-			} while (wpid != pid);
-		}
-	}
-}
-/**
- * main - entry point
- * @argc: arg count
- * @argv: array of pointers
- * @envp: environment vars
- * Return: 0
- */
-int main(__attribute__((unused)) int argc, char *argv[], char *envp[])
-{
-	char *input = NULL;
-	size_t len = 0;
-
-	signal(SIGINT, handleSig);
-
-	while (1)
-	{
-		if (isatty(STDIN_FILENO))
-		{
-			display_prompt();
-		}
-
-		if (getline(&input, &len, stdin) == -1)
-		{
-			if (isatty(STDIN_FILENO))
-			{
-				write(STDOUT_FILENO, "\n", 1);
-			}
-			free(input);
-			break;
-		}
-
-		input[strcspn(input, "\n")] = '\0';
-
-		if (strcmp(input, "env") == 0)
-		{
-			print_env(envp);
-		}
-		else
-		{
-			exeCmd(input, argv[0], envp);
-		}
-	}
-
+	clearEnvironmentAndExecuteEnv();
 	return (0);
 }
